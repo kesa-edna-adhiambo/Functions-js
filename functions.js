d6baffafd028d0b3bff09394eed9185db6d81b90
kesa-edna-adhiambo <edna@gmail.com>
2024-03-29 20:21:13 +0300

    functions

--- functions.js
+++ functions.js
@@ -1,4 +1,57 @@
 //Question 1
+//ascending
+let elements =[
+    {
+    "name": "kate",
+    },
+
+    {
+      "name": "Faith",
+    },
+
+    {
+      "name":"Brenda",
+    },
+
+    {
+        "name":"Edna"
+    },
+
+]
+console.log("original elements are:")
+console.log(elements)
+let sortedElements = elements.sort(
+    (n1 , n2) => (n1.name < n2.name) ? -1 : (n1.name > n2.name)? 1 : 0)
+
+console.log("names sorted in ascending order")
+console.log(sortedElements)
+
+//descending
+let ele =[
+    {
+    "name": "kate",
+    },
+
+    {
+      "name": "Faith",
+    },
+
+    {
+      "name":"Brenda",
+    },
+
+    {
+        "name":"Edna"
+    },
+
+]
+console.log("original ele are:")
+console.log(ele)
+let sortedEle = ele.sort(
+    (n1 , n2) => (n1.name < n2.name) ? 1 : (n1.name > n2.name)? -1 : 0)
+
+console.log("names sorted in descending order")
+console.log(sortedEle)
 
 
 
@@ -18,6 +71,38 @@ function myFunction(arr2){
 console.log(myFunction(arr2))
 
 //Question 3
+//ascending order
+let object = [
+    {
+        "id": 40486,
+        "name": "Brenda",
+        "salary":4000
+    },
+
+    {
+        "id": 32323,
+        "name":"Faith",
+        "salary":3000
+    },
+
+    {
+        "id": 20224,
+        "name": "Levin",
+        "salary": 5000
+    },
+
+]
+
+
+console.log("Original object are:")
+console.log(object)
+let sortedSalary = object.sort(
+     (s1, s2) => (s1.salary < s2.salary) ? -1 : (s1.salary > s2.salary) ? 1:0);
+
+     console.log("object sorted based on ascending order of their salary")
+     console.log(sortedSalary)
+   
+
 
 //Question4
 function multiply(numbers) {
@@ -28,13 +113,30 @@ function multiply(numbers) {
 multiply([3, 8, 4, 6])
 
 //Qustion5
-function changeArray(arr){
-    const firstFour = arr.slice(0,4).map(item => item * 8)
-    const lastTwo = arr.slice(-2).map(item => item +5)
-    const newArr = firstFour.concatenate(lastTwo);
-    console.log(newArr);
+
+function array(arr){
+    let firstFour = arr.slice(0, 5).map(item => item * 8)
+    let lastTwo = arr.slice(-3).map(item => item + 5)
+    let newArr = (firstFour) +  (lastTwo)
+    console.log(newArr)
+
 }
-changeArray([9, 10, 11, 12, 14, 2, 1])
+
+array([9, 8, 7, 6, 5, 4, 3, 2])
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
 
 
 
